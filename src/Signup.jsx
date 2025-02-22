ffaa3f55a04693a27829f6bec0134900eb6e09a7
lemibk <lemibidikasenbeta@gmail.com>
2025-02-22 23:00:58 +0300

    Starting the signup page

---
+++ src/Signup.jsx
@@ -0,0 +1,16 @@
+// src/About.js
+import React from 'react';
+import './Signup.css'
+const Signup = () => {
+  return (
+    <>
+    
+    <h1>&nbsp;Signup Page</h1>
+    <div className='center'>
+      
+      <p id='Text'> &nbsp;&nbsp;&nbsp;&nbsp; Welcome <br />&nbsp;&nbsp;Register with us <br />Let's Grow Together</p>
+    </div></>
+  );
+};
+
+export default Signup;
