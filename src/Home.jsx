add991de2bcfc1f2db72816ae9cb48529976b7da
lemibk <lemibidikasenbeta@gmail.com>
2025-02-22 23:04:48 +0300

    m\starting the Home page

---
+++ src/Home.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Home.css'
+const Home = () => {
+  return (
+    <>
+      
+        <h1 >Home Page</h1>
+        
+      
+      
+      <section style={styles.section}>
+        <p style={styles.paragraph}>Welcome to the Wolkite University Student Facility Complaint Management System.
+           <br /> This platform streamlines facility issue reporting and ensures quick resolutions <br />
+         Together, we create a better campus experience for all.</p>
+      </section>
+      
+      <section style={styles.section}>
+        <p>This is the contact section</p>
+        
+      </section>
+    </>
+  );
+};
+
+const styles = {
+  section: {
+    height: '100vh',  // Full screen height
+    width: '100%',     // Full width
+    display: 'flex',   // Make it a flex container
+    justifyContent: 'center',  // Center horizontally
+    marginTop:'50px',
+   
+  },
+  paragraph:{
+    fontFamily: "Times New Roman",
+    fontSize:'26px'
+    },
+  center:{
+alignItems:'center',
+justifyContent:'center',
+    }
+};
+
+export default Home;
