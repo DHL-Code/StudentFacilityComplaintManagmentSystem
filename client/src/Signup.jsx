8159423895d1fd4fa4f420b3852da49c00452667
DINKNEHTEMESGEN <gjhomdinknehtemesgen@gmail.com>
2025-02-24 15:15:57 +0300

    signup page with it's css

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -1,14 +1,38 @@
-// src/About.js
+// src/Contact.js
 import React from 'react';
-import './Signup.css'
+import './Signup.css';
+
 const Signup = () => {
-  return (
-    <>
-    
-    <h1>&nbsp;Signup Page</h1>
-    <div className='center'>
-      
-      <p id='Text'> &nbsp;&nbsp;&nbsp;&nbsp; Welcome <br />&nbsp;&nbsp;Register with us <br />Let's Grow Together</p>
+ 
+  return (<>
+    <h1 >Welcome</h1>
+    <div >
+      <div className="inline-heading">
+          <input type="text" placeholder='FirstName' className='inline-input' />
+        </div>
+        <div className="inline-heading">
+          <input type="text" placeholder='LastName' className='inline-input' />
+        </div>
+        <div className="inline-heading">
+          <input type="text" placeholder="UserID" className="inline-input"/>
+        </div> 
+        <div className="inline-heading">
+          <input type="text" placeholder='Department' className='inline-input' />
+        </div>
+        <div className="inline-heading">
+          <input type="text" placeholder='Phone Number' className='inline-input' />
+        </div>
+        <div className="inline-heading">
+          <input type="email" placeholder='Email' className='inline-input' />
+        </div>
+        <div className="inline-heading">
+          <input type="password" placeholder='Password' className='inline-input' />
+        </div>
+        <div className="inline-heading">
+          <input type="password" placeholder='Confirm Password' className='inline-input' />
+        </div>
+        <input className='button' type="button" value="Signup" />
+       
     </div></>
   );
 };
